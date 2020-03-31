543e694e33bcc59bf839f8a0a43fcd5e47febda1
thebrothersjourney <info@thebrothersjourney.com>
2020-04-01 08:43:35 +1100

    Updated configuration

--- config.php
+++ config.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Str;
 return [
     'baseUrl' => '',
     'production' => false,
-    'siteName' => 'Blog Starter Template',
-    'siteDescription' => 'Generate an elegant blog with Jigsaw',
-    'siteAuthor' => 'Author Name',
+    'siteName' => 'The Brother\'s Journey',
+    'siteDescription' => 'Growing the planted seeds',
+    'siteAuthor' => 'Quoc',
 
     // collections
     'collections' => [
         'posts' => [
-            'author' => 'Author Name', // Default author, if not provided in a post
+            'author' => 'Quoc', // Default author, if not provided in a post
             'sort' => '-date',
             'path' => 'blog/{filename}',
         ],
